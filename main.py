1e8d7ff7627397c018a51662f7e9eb41fb6d4834
Bastienlsg <bastien.lassaigne@gmail.com>
2022-04-06 14:32:06 +0200

    évolution

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from wtforms import StringField, SubmitField, BooleanField, IntegerField, \
 from wtforms.validators import DataRequired
 from flask import Flask, render_template, redirect, request, session, g
 from datetime import datetime, timedelta
-#from factures import *
+# from factures import *
 import os
 
 app = Flask(__name__)
@@ -17,11 +17,13 @@ Bootstrap(app)
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv('.env')
 
+
 class Utilisateur:
     def __init__(self, id, pseudo, mdp):
         self.id = id
         self.pseudo = pseudo
         self.mdp = mdp
+
 
 # tableau contenant la liste des utilisateurs avec le droit d'acceder au site
 db = DBSingleton.Instance()
@@ -30,8 +32,7 @@ utilisateurs = []
 for i in users:
     user = Utilisateur(id=i[0], pseudo=i[1], mdp=i[2])
     # globals()["Utilisateur%s" % str(i[0])] = Utilisateur(id=i[0], pseudo=i[1], mdp=i[2])
-    utilisateurs.append(user) #eval("Utilisateur%s" % i[0]))
-
+    utilisateurs.append(user)  # eval("Utilisateur%s" % i[0]))
 
 
 @app.before_request
@@ -39,11 +40,13 @@ def before_request():
     g.utilisateur = False
     if 'utilisateur_id' in session:
         utilisateur = [x for x in utilisateurs if x.id == session['utilisateur_id']]
-        g.utilisateur = utilisateur[0] #get_user_from_db(session['pseudo'])
+        g.utilisateur = utilisateur[0]  # get_user_from_db(session['pseudo'])
 
 
 @app.route('/', methods=['GET', 'POST'])
 def connexion():
+    form = LoginForm()
+    result = render_template('index.html', form=form, title='connexion')
     if request.method == 'POST':
         session.pop('utilisateur_id', None)
         pseudo = request.form['pseudo']
@@ -59,9 +62,8 @@ def connexion():
                 session['pseudo'] = utilisateur[0].pseudo
                 instant = datetime.now()
                 session['heure_connexion'] = instant
-                return redirect('/accueil')
-    form = LoginForm()
-    return render_template('index.html', form=form, title='connexion')
+                result = redirect('/accueil')
+    return result
 
 
 @app.route('/deconnexion')
@@ -70,16 +72,16 @@ def deconnexion():
     return redirect('/')
 
 
-
 @app.route('/accueil')
 def accueil():
-    if not g.utilisateur:
-        return redirect('/')
     connexion_unique = DBSingleton.Instance()
     element = "SELECT prospect.nom, COUNT(facture.id) AS 'Nombre de facture' FROM `prospect` LEFT JOIN facture ON " \
               "facture.prospect_id = prospect.id GROUP BY prospect.nom ORDER BY prospect.nom ASC "
     prospects = connexion_unique.fetchall_simple(element)
-    return render_template('accueil.html', prospects=prospects)
+    result = render_template('accueil.html', prospects=prospects)
+    if not g.utilisateur:
+        result = redirect('/')
+    return result
 
 
 class BarreDeRechercheFiltre(FlaskForm):
@@ -89,11 +91,6 @@ class BarreDeRechercheFiltre(FlaskForm):
 
 @app.route('/menu-entreprises/<prospect>/<statut>/<recherche>', methods=['GET', 'POST'])
 def prospect(prospect, statut, recherche):
-    if not g.utilisateur:
-        return redirect('/')
-    filtre = BarreDeRechercheFiltre()
-    if filtre.valider.data == True:
-        return redirect("/menu-entreprises/" + prospect + "/" + statut + "/" + filtre.filtreDefini.data)
     # filtre en fonction du statut
     if statut == "inactif":
         bool_statut = 0
@@ -103,7 +100,8 @@ def prospect(prospect, statut, recherche):
     if recherche == "filtre-off":
         sqlfiltre = ""
     else:
-        sqlfiltre = "and (contact.`nom` LIKE '" + recherche + "%' or contact.`email` LIKE '" + recherche + "%' or contact.`prenom` LIKE '" + recherche + "%')"
+        sqlfiltre = "and (contact.`nom` LIKE '" + recherche + "%' or contact.`email` LIKE '" + recherche + \
+                    "%' or contact.`prenom` LIKE '" + recherche + "%')"
     connexion_unique = DBSingleton.Instance()
     sql = "SELECT contact.*, COUNT(commentaire.contact_id) AS 'Nombre de commentaire' FROM `contact` LEFT JOIN " \
           "commentaire ON commentaire.contact_id = contact.id JOIN prospect ON prospect.id = contact.prospect_id " \
@@ -114,36 +112,52 @@ def prospect(prospect, statut, recherche):
                 "prospect.description, prospect.url FROM `prospect` WHERE prospect.nom = '%s' " % prospect
     info_prospect = connexion_unique.fetchall_simple(element_2)
     activiter = bool_statut
-    return render_template('prospect.html', contacts=contacts, nom_prospect=prospect, info_prospect=info_prospect,
-                           activiter=activiter, barrederecherche=filtre)
+    filtre = BarreDeRechercheFiltre()
+    result = render_template('prospect.html', contacts=contacts, nom_prospect=prospect, info_prospect=info_prospect,
+                             activiter=activiter, barrederecherche=filtre)
+    if not g.utilisateur:
+        result = redirect('/')
+    if filtre.valider.data == True:
+        result = redirect("/menu-entreprises/" + prospect + "/" + statut + "/" + filtre.filtreDefini.data)
+    return result
 
 
 @app.route('/contact/<prospect>/<contact>')
 def contact(contact, prospect):
-    if not g.utilisateur:
-        return redirect('/')
     connexion_unique = DBSingleton.Instance()
-    element = "SELECT contact.*, COUNT(commentaire.contact_id) AS 'Nombre de commentaire' FROM `contact` LEFT JOIN commentaire ON commentaire.contact_id = contact.id JOIN prospect ON prospect.id = contact.prospect_id WHERE prospect.nom = '%s' AND statut = '0' GROUP BY contact.nom" % prospect
+    element = "SELECT contact.*, COUNT(commentaire.contact_id) AS 'Nombre de commentaire' FROM `contact` LEFT JOIN " \
+              "commentaire ON commentaire.contact_id = contact.id JOIN prospect ON prospect.id = contact.prospect_id " \
+              "WHERE prospect.nom = '%s' AND statut = '0' GROUP BY contact.nom" % prospect
     contacts = connexion_unique.fetchall_simple(element)
-    element_2 = "SELECT commentaire.description, commentaire.date_creation, utilisateur.login FROM `contact`JOIN commentaire ON commentaire.contact_id = contact.id JOIN utilisateur ON utilisateur.id = commentaire.utilisateur_id WHERE contact.id = '%s' ORDER BY date_creation DESC LIMIT 1" % contact
+    element_2 = "SELECT commentaire.description, commentaire.date_creation, utilisateur.login FROM `contact`JOIN " \
+                "commentaire ON commentaire.contact_id = contact.id JOIN utilisateur ON utilisateur.id = " \
+                "commentaire.utilisateur_id WHERE contact.id = '%s' ORDER BY date_creation DESC LIMIT 1" % contact
     commentaire = connexion_unique.fetchall_simple(element_2)
-    element_3 = "SELECT commentaire.description, commentaire.date_creation, utilisateur.login FROM `contact`JOIN commentaire ON commentaire.contact_id = contact.id JOIN utilisateur ON utilisateur.id = commentaire.utilisateur_id WHERE contact.id = '%s' ORDER BY date_creation DESC LIMIT 100 OFFSET 1 " % contact
+    element_3 = "SELECT commentaire.description, commentaire.date_creation, utilisateur.login FROM `contact`JOIN " \
+                "commentaire ON commentaire.contact_id = contact.id JOIN utilisateur ON utilisateur.id = " \
+                "commentaire.utilisateur_id WHERE contact.id = '%s' ORDER BY date_creation DESC LIMIT 100 OFFSET 1 " \
+                % contact
     liste_commentaires = connexion_unique.fetchall_simple(element_3)
-    return render_template('contact.html', contacts=contacts, nom_prospect=prospect, contact=contact,
-                           commentaire=commentaire, liste_commentaires=liste_commentaires)
+    result = render_template('contact.html', contacts=contacts, nom_prospect=prospect, contact=contact,
+                             commentaire=commentaire, liste_commentaires=liste_commentaires)
+    if not g.utilisateur:
+        result = redirect('/')
+
+    return result
 
 
 @app.route('/changer-statut/<prospect>/<contact_id>')
 def changement_statut(prospect, contact_id):
     if not g.utilisateur:
-        return redirect('/')
+        result = redirect('/')
     statut_actuel = select("contact", "statut", "id", contact_id)[0][0]
     if statut_actuel == 0:
         update("contact", "statut", (1,), "id", contact_id)
-        return redirect("/menu-entreprises/" + prospect + "/actif/filtre-off")
+        result = redirect("/menu-entreprises/" + prospect + "/actif/filtre-off")
     if statut_actuel == 1:
         update("contact", "statut", (0,), "id", contact_id)
-        return redirect("/menu-entreprises/" + prospect + "/inactif/filtre-off")
+        result = redirect("/menu-entreprises/" + prospect + "/inactif/filtre-off")
+    return result
 
 
 class FormulaireFacturation(FlaskForm):
@@ -151,10 +165,11 @@ class FormulaireFacturation(FlaskForm):
     valider = SubmitField('Valider')
 
 
-#@app.route('/generer_facture/<prospect_nom>/<contact_id>', methods=['GET', 'POST'])
+# @app.route('/generer_facture/<prospect_nom>/<contact_id>', methods=['GET', 'POST'])
 def genration_factures(prospect_nom, contact_id):
+    result = render_template("apercufacture.html", )
     if not g.utilisateur:
-        return redirect('/')
+        result = redirect('/')
     formFacture = FormulaireFacturation()
     if formFacture.valider.data == True:
         prospect_id = select("prospect", "id", "nom", prospect_nom)[0][0]
@@ -178,14 +193,15 @@ def genration_factures(prospect_nom, contact_id):
         facture = Facture(entreprise, prospect, contact, details_facture)
         facture.generate()
         nom_facture = facture.nomFacture
-        return redirect("/apercu_facture/" + nom_facture)
-    return render_template("apercufacture.html", )
+        result = redirect("/apercu_facture/" + nom_facture)
+    return result
 
 
 @app.route('/apercu_factures/<nom_prospect>/<id_contact>', methods=['GET', 'POST'])
 def apercu_factures(nom_prospect, id_contact):
+    result = render_template("apercufacture.html", formFacture=formFacture, liste_url=liste_url)
     if not g.utilisateur:
-        return redirect('/')
+        result = redirect('/')
     liste_url = []
     i = 0
     for _ in select("facture", "id", "contact_id", id_contact):
@@ -219,33 +235,32 @@ def apercu_factures(nom_prospect, id_contact):
         facture = Facture(entreprise, prospect, contact, details_facture)
         facture.generate()
         nom_facture = facture.nomFacture
-        return redirect("/apercu_facture/" + nom_facture)
-    return render_template("apercufacture.html", formFacture=formFacture,liste_url=liste_url)
+        result = redirect("/apercu_facture/" + nom_facture)
+    return result
 
 
 @app.route('/apercu_facture/<nom_facture>', methods=['GET', 'POST'])
 def apercu_facture(nom_facture):
-    if not g.utilisateur:
-        return redirect('/')
     url_pdf = os.environ.get("url_dossier_factures") + nom_facture + ".pdf"
     liste_url = [url_pdf]
-    return render_template("apercufacture.html", liste_url=liste_url)
+    result = render_template("apercufacture.html", liste_url=liste_url)
+    if not g.utilisateur:
+        result = redirect('/')
+    return result
 
 
 @app.route('/effacer-prospect/<prospect>')
 def effacer_prospect(prospect):
+    result = redirect("/accueil")
     if not g.utilisateur:
-        return redirect('/')
+        result = redirect('/')
     id_prospect = select("prospect", "id", "nom", prospect)[0][0]
     delete("prospect", "id", id_prospect)
-    return redirect("/accueil")
+    return result
 
 
 @app.route('/ajouter-prospect', methods=['GET', 'POST'])
 def ajouter_prospect():
-    if not g.utilisateur:
-        return redirect('/')
-
     class Ajouterprospect(FlaskForm):
         nom = StringField('nom', validators=[DataRequired()])
         siret = DecimalField('siret', validators=[DataRequired()])
@@ -254,8 +269,10 @@ def ajouter_prospect():
         ville = StringField('ville', validators=[DataRequired()])
         description = TextAreaField('description')
         url = TextAreaField('url')
-
     form = Ajouterprospect()
+    result = render_template('ajouter-prospect.html', form=form)
+    if not g.utilisateur:
+        result = redirect('/')
     if form.validate_on_submit():
         nom = request.form['nom']
         siret = request.form['siret']
@@ -266,15 +283,12 @@ def ajouter_prospect():
         url = request.form['url']
         params: tuple = (nom, siret, adresse, codepostal, ville, description, url)
         Insert("prospect", "nom, numero_siret, adresse_postale, code_postal, ville, description, url", params)
-        return redirect("/accueil")
-    return render_template('ajouter-prospect.html', form=form)
+        result = redirect("/accueil")
+    return result
 
 
 @app.route('/ajouter-contact/<prospect>', methods=['GET', 'POST'])
 def ajouter_contact(prospect):
-    if not g.utilisateur:
-        return redirect('/')
-
     class Ajoutercontact(FlaskForm):
         nom = StringField('nom', validators=[DataRequired()])
         prenom = StringField('prenom', validators=[DataRequired()])
@@ -283,6 +297,10 @@ def ajouter_contact(prospect):
         telephone = TelField('telephone')
 
     form = Ajoutercontact()
+    result = render_template('ajouter-contact.html', form=form)
+    if not g.utilisateur:
+        result = redirect('/')
+
     if form.validate_on_submit():
         nom = request.form['nom']
         prenom = request.form['prenom']
@@ -293,27 +311,26 @@ def ajouter_contact(prospect):
         id_prospect = select("prospect", "id", "nom", prospect)[0][0]
         params: tuple = (nom, prenom, email, poste, telephone, statut, id_prospect)
         Insert("contact", "nom, prenom, email, poste, telephone, statut, prospect_id", params)
-        return redirect("/menu-entreprises/" + prospect + "/actif/filtre-off")
-    return render_template('ajouter-contact.html', form=form)
-
+        result = redirect("/menu-entreprises/" + prospect + "/actif/filtre-off")
+    return result
 
 @app.route('/ajouter-commentaire/<prospect>/<id_contact>', methods=['GET', 'POST'])
 def ajouter_commentaire(prospect, id_contact):
-    if not g.utilisateur:
-        return redirect('/')
-
     class Ajoutercommentaire(FlaskForm):
         commentaire = TextAreaField('commentaire', validators=[DataRequired()])
-
     form = Ajoutercommentaire()
+    result = render_template('ajouter-commentaire.html', form=form)
+    if not g.utilisateur:
+        result = redirect('/')
+
     if form.validate_on_submit():
         commentaire = request.form['commentaire']
         contact = id_contact
         utilisateur = session['utilisateur_id']
         params: tuple = (utilisateur, contact, commentaire)
         Insert("commentaire", "utilisateur_id, contact_id, description", params)
-        return redirect("/contact/" + prospect + "/" + id_contact)
-    return render_template('ajouter-commentaire.html', form=form)
+        result = redirect("/contact/" + prospect + "/" + id_contact)
+    return result
 
 
 if __name__ == "__main__":
